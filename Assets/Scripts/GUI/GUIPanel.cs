170357fbdcfd6c48aba2beb7e0b71cea584d4a4f
Jacob van't Hoog <vantjac@gmail.com>
2017-11-16 20:42:57 -0800

    Prevent pressing buttons while scrolling; hide scroll bars

--- Assets/Scripts/GUI/GUIPanel.cs
+++ Assets/Scripts/GUI/GUIPanel.cs
@@ -17,6 +17,8 @@ public class GUIPanel : MonoBehaviour {
     public float scaledScreenWidth;
     public int depth = 0;
 
+    private bool touchMoved = false;
+
     public void OnEnable()
     {
         openPanels.Add(this);
@@ -32,12 +34,32 @@ public class GUIPanel : MonoBehaviour {
         if (globalGUISkin == null)
             globalGUISkin = guiSkin;
 
-        GUI.skin = guiSkin;
+        GUI.skin = globalGUISkin;
         GUI.depth = 1;
+        GUI.enabled = true;
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+                touchMoved = true;
+            if (touchMoved && PanelContainsPoint(touch.position))
+                GUI.enabled = false;
+        }
+        else
+        {
+            touchMoved = false;
+        }
 
         scaleFactor = Screen.height / targetHeight;
         GUI.matrix = Matrix4x4.Scale(new Vector3(scaleFactor, scaleFactor, 1));
         scaledScreenWidth = Screen.width / scaleFactor;
+    }
+
+    public bool PanelContainsPoint(Vector2 point)
+    {
+        point.y = Screen.height - point.y;
+        point /= scaleFactor;
+        return panelRect.Contains(point);
     }
 
     public static GUIPanel PanelContainingPoint(Vector2 point)
@@ -45,13 +67,10 @@ public class GUIPanel : MonoBehaviour {
         if (openPanels.Count == 0)
             return null;
 
-        point.y = Screen.height - point.y;
-        point /= openPanels[0].scaleFactor;
-
         GUIPanel match = null;
         foreach (GUIPanel panel in openPanels)
         {
-            if (panel.panelRect.Contains(point))
+            if (panel.PanelContainsPoint(point))
             {
                 if (match == null || panel.depth < match.depth)
                     match = panel;
