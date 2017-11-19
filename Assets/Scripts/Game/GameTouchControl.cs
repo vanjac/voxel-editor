2bf3a52a9cf9a7a141c954d14bd0247c74b3f660
Jacob van't Hoog <vantjac@gmail.com>
2017-11-18 18:28:48 -0800

    Start play scene with first person control

---
+++ Assets/Scripts/Game/GameTouchControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameTouchControl : MonoBehaviour {
+
+    CrossPlatformInputManager.VirtualAxis hAxis, vAxis;
+
+	void OnEnable () {
+        hAxis = new CrossPlatformInputManager.VirtualAxis("Mouse X");
+        CrossPlatformInputManager.RegisterVirtualAxis(hAxis);
+        vAxis = new CrossPlatformInputManager.VirtualAxis("Mouse Y");
+        CrossPlatformInputManager.RegisterVirtualAxis(vAxis);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            {
+                hAxis.Update(touch.deltaPosition.x / 10);
+                vAxis.Update(touch.deltaPosition.y / 10);
+            }
+            else
+            {
+                hAxis.Update(0);
+                vAxis.Update(0);
+            }
+        }
+        else
+        {
+            hAxis.Update(0);
+            vAxis.Update(0);
+        }
+	}
+}
