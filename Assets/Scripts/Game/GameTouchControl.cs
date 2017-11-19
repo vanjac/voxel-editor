44e1f92801e3629368c22c26735f5da59c90a417
Jacob van't Hoog <vantjac@gmail.com>
2017-11-18 19:44:01 -0800

    Moving joystick doesn't move camera

--- Assets/Scripts/Game/GameTouchControl.cs
+++ Assets/Scripts/Game/GameTouchControl.cs
@@ -7,6 +7,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameTouchControl : MonoBehaviour {
 
     CrossPlatformInputManager.VirtualAxis hAxis, vAxis;
+    bool uiInteraction = false;
 
 	void OnEnable () {
         hAxis = new CrossPlatformInputManager.VirtualAxis("Mouse X");
@@ -20,7 +21,9 @@ public class GameTouchControl : MonoBehaviour {
         if (Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
-            if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                uiInteraction = true;
+            if(!uiInteraction)
             {
                 hAxis.Update(touch.deltaPosition.x / 10);
                 vAxis.Update(touch.deltaPosition.y / 10);
@@ -33,6 +36,7 @@ public class GameTouchControl : MonoBehaviour {
         }
         else
         {
+            uiInteraction = false;
             hAxis.Update(0);
             vAxis.Update(0);
         }
