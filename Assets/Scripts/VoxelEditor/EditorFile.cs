ba690bed406318edb8091a5312df53f4d85558f1
Jacob van't Hoog <vantjac@gmail.com>
2017-11-24 11:52:56 -0800

    EditorFile: only save if unsaved changes, log application events

--- Assets/Scripts/VoxelEditor/EditorFile.cs
+++ Assets/Scripts/VoxelEditor/EditorFile.cs
@@ -14,33 +14,45 @@ public class EditorFile : MonoBehaviour
         Debug.Log("Loading " + mapName);
         MapFileReader reader = new MapFileReader(mapName);
         reader.Read(cameraPivot, voxelArray, true);
+        // reading the file creates new voxels which sets the unsavedChanges flag
+        voxelArray.unsavedChanges = false;
     }
 
     public void Save()
     {
+        if (!voxelArray.unsavedChanges)
+        {
+            Debug.Log("No unsaved changes");
+            return;
+        }
         Debug.Log("Saving...");
         MapFileWriter writer = new MapFileWriter(SelectedMap.GetSelectedMapName());
         writer.Write(cameraPivot, voxelArray);
+        voxelArray.unsavedChanges = false;
     }
 
     public void LoadScene(string name)
     {
+        Debug.Log("LoadScene(" + name + ")");
         Save();
         SceneManager.LoadScene(name);
     }
 
     void OnEnable()
     {
+        Debug.Log("OnEnable()");
         Load();
     }
 
     void OnApplicationQuit()
     {
+        Debug.Log("OnApplicationQuit()");
         Save();
     }
 
     void OnApplicationPause()
     {
+        Debug.Log("OnApplicationPause()");
         Save();
     }
 }
