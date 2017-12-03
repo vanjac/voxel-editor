9403256bc2aaf9441c1057e20c6f9c4c0ee18dab
Jacob van't Hoog <vantjac@gmail.com>
2017-12-03 10:41:12 -0800

    Entity interface

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Entity
+{
+    string EntityTypeName();
+
+    bool SupportsTag();
+    byte GetTag();
+    void SetTag(byte tag);
+
+    string[] PropertyNames();
+    string GetProperty(string name);
+    void SetProperty(string name, string value);
+    string PropertyGUI(string name, string value);
+
+    string[] ActionNames();
+    string ActionArgumentGUI(string name, string value);
+
+    string[] EventNames();
+    bool EventHasActivator();
+
+    List<Output> OutputList(); // can be null if outputs not supported
+
+    List<Entity> BehaviorList(); // can be null if behaviors not supported
+}
+
+public struct Output
+{
+    Entity targetEntity; // null for Self or Activator
+    bool targetEntityIsActivator;
+    string targetAction;
+    string actionArgument;
+
+    // activator rule...
+    bool[] activatorTagsAllowed;
+    Entity[] activatorEntityList;
+    bool activatorEntityBlacklist;
+    string[] activatorTypeList; // also applies to behaviors
+    bool activatorTypeBlacklist;
+}
