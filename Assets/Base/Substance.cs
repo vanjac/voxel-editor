e98d3ce2520ba78747333a7f8207c674f3a278b0
Jacob van't Hoog <vantjac@gmail.com>
2017-12-18 19:31:45 -0800

    Sensor settings work

--- Assets/Base/Substance.cs
+++ Assets/Base/Substance.cs
@@ -52,13 +52,53 @@ public class Substance : DynamicEntity
 
 public class SubstanceComponent : MonoBehaviour
 {
+    private enum SensorCycle
+    {
+        OFF, TURNING_ON, ON, TURNING_OFF, TIMED_OUT
+    }
+
+    private static bool SensorOn(SensorCycle cycle)
+    {
+        return cycle == SensorCycle.TURNING_ON
+            || cycle == SensorCycle.ON
+            || cycle == SensorCycle.TIMED_OUT;
+    }
+
+    private static bool BehaviorsOn(SensorCycle cycle)
+    {
+        return cycle == SensorCycle.ON || cycle == SensorCycle.TURNING_OFF;
+    }
+
     public Substance substance;
 
     private List<Behaviour> offComponents = new List<Behaviour>();
     private List<Behaviour> onComponents = new List<Behaviour>();
 
     private SensorComponent sensorComponent;
-    private bool sensorWasOn;
+    private SensorCycle _sensorCycle;
+    private SensorCycle sensorCycle
+    {
+        get
+        {
+            return _sensorCycle;
+        }
+        set
+        {
+            if (SensorOn(value) && !SensorOn(_sensorCycle))
+                t_sensorOn = Time.time;
+            if (BehaviorsOn(value) && !BehaviorsOn(_sensorCycle))
+            {
+                SetBehaviors(true);
+                t_behaviorsOn = Time.time;
+            }
+            else if(!BehaviorsOn(value) && BehaviorsOn(_sensorCycle))
+                SetBehaviors(false);
+            _sensorCycle = value;
+        }
+    }
+    private float t_behaviorsOn;
+    private float t_sensorOn;
+
 
     void Start()
     {
@@ -79,7 +119,7 @@ public class SubstanceComponent : MonoBehaviour
 
         if (substance.sensor != null)
             sensorComponent = substance.sensor.MakeComponent(gameObject);
-        sensorWasOn = false;
+        sensorCycle = SensorCycle.OFF;
         foreach (EntityBehavior behavior in substance.behaviors)
         {
             Behaviour c = behavior.MakeComponent(gameObject);
@@ -97,21 +137,59 @@ public class SubstanceComponent : MonoBehaviour
     {
         if (sensorComponent == null)
             return;
-        bool sensorIsOn = sensorComponent.isOn();
-        if (sensorIsOn && !sensorWasOn)
+        bool sensorIsOn = sensorComponent.isOn() ^ substance.sensor.invert;
+
+        // change cycle state based on sensor
+        switch (sensorCycle)
         {
-            foreach (Behaviour onComponent in onComponents)
-                onComponent.enabled = true;
-            foreach (Behaviour offComponent in offComponents)
-                offComponent.enabled = false;
+            case SensorCycle.OFF:
+                if (sensorIsOn)
+                    sensorCycle = SensorCycle.TURNING_ON;
+                break;
+            case SensorCycle.TURNING_ON:
+                if (!sensorIsOn)
+                    sensorCycle = SensorCycle.OFF;
+                break;
+            case SensorCycle.ON:
+                if (!sensorIsOn)
+                    sensorCycle = SensorCycle.TURNING_OFF;
+                break;
+            case SensorCycle.TURNING_OFF:
+                if (sensorIsOn)
+                    sensorCycle = SensorCycle.ON;
+                break;
+            case SensorCycle.TIMED_OUT:
+                if (!sensorIsOn)
+                    sensorCycle = SensorCycle.OFF;
+                break;
         }
-        else if (!sensorIsOn && sensorWasOn)
+
+        float time = Time.time;
+
+        // change cycle state based on time
+        switch (sensorCycle)
         {
-            foreach (Behaviour onComponent in onComponents)
-                onComponent.enabled = false;
-            foreach (Behaviour offComponent in offComponents)
-                offComponent.enabled = true;
+            case SensorCycle.TURNING_ON:
+                if (time - t_sensorOn > substance.sensor.turnOnTime)
+                    sensorCycle = SensorCycle.ON;
+                break;
+            case SensorCycle.ON:
+                if (time - t_behaviorsOn > substance.sensor.maxOnTime)
+                    sensorCycle = SensorCycle.TIMED_OUT;
+                break;
+            case SensorCycle.TURNING_OFF:
+                // timeSinceChange is time since ON, not time since TURNING_OFF
+                if (time - t_behaviorsOn > substance.sensor.minOnTime)
+                    sensorCycle = SensorCycle.OFF;
+                break;
         }
-        sensorWasOn = sensorIsOn;
+    } // Update()
+
+    private void SetBehaviors(bool on)
+    {
+        foreach (Behaviour onComponent in onComponents)
+            onComponent.enabled = on;
+        foreach (Behaviour offComponent in offComponents)
+            offComponent.enabled = !on;
     }
 }
