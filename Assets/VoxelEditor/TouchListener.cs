2475f1ed9f93ba9b82b35c1f43e2c86fea3217f6
Jacob van't Hoog <vantjac@gmail.com>
2018-02-23 19:43:49 -0800

    Normalized rotate gesture for different resolutions

--- Assets/VoxelEditor/TouchListener.cs
+++ Assets/VoxelEditor/TouchListener.cs
@@ -19,6 +19,7 @@ public class TouchListener : MonoBehaviour
     public TouchOperation currentTouchOperation = TouchOperation.NONE;
     public MoveAxis movingAxis;
     private Transform pivot;
+    private Camera camera;
 
     private Voxel lastHitVoxel;
     private int lastHitFaceI;
@@ -26,6 +27,7 @@ public class TouchListener : MonoBehaviour
     void Start()
     {
         pivot = transform.parent;
+        camera = GetComponent<Camera>();
     }
 
     void Update ()
@@ -41,7 +43,7 @@ public class TouchListener : MonoBehaviour
             Touch touch = Input.GetTouch(0);
 
             RaycastHit hit;
-            bool rayHitSomething = Physics.Raycast(GetComponent<Camera>().ScreenPointToRay(Input.GetTouch(0).position), out hit);
+            bool rayHitSomething = Physics.Raycast(camera.ScreenPointToRay(Input.GetTouch(0).position), out hit);
             Voxel hitVoxel = null;
             int hitFaceI = -1;
             MoveAxis hitMoveAxis = null;
@@ -159,9 +161,11 @@ public class TouchListener : MonoBehaviour
             }
 
             Vector3 move = (touchZero.deltaPosition + touchOne.deltaPosition) / 2;
+            move *= 300f;
+            move /= camera.pixelHeight;
             Vector3 pivotRotationEuler = pivot.rotation.eulerAngles;
-            pivotRotationEuler.y += move.x * 0.3f;
-            pivotRotationEuler.x -= move.y * 0.3f;
+            pivotRotationEuler.y += move.x;
+            pivotRotationEuler.x -= move.y;
             if (pivotRotationEuler.x > 90 && pivotRotationEuler.x < 180)
                 pivotRotationEuler.x = 90;
             if (pivotRotationEuler.x < -90 || (pivotRotationEuler.x > 180 && pivotRotationEuler.x < 270))
@@ -182,7 +186,7 @@ public class TouchListener : MonoBehaviour
             for (int i = 0; i < 3; i++)
                 move += Input.GetTouch(i).deltaPosition;
             move *= 4.0f;
-            move /= GetComponent<Camera>().pixelHeight;
+            move /= camera.pixelHeight;
             pivot.position -= move.x * pivot.right * pivot.localScale.z;
             pivot.position -= move.y * pivot.up * pivot.localScale.z;
         }
@@ -190,7 +194,6 @@ public class TouchListener : MonoBehaviour
 
     private void UpdateZoomDepth()
     {
-        Camera camera = GetComponent<Camera>();
         // adjust the depth of the pivot point to the depth at the average point between the fingers
         int touchCount = Input.touchCount;
         Vector2 avg = Vector2.zero;
