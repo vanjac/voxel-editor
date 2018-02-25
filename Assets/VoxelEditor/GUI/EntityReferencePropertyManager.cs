9fa969814b8290e3a443967221e76ea2e0c0a33e
Jacob van't Hoog <vantjac@gmail.com>
2018-02-24 20:09:48 -0800

    EntityReferenceLine to manage lines to entities

--- Assets/VoxelEditor/GUI/EntityReferencePropertyManager.cs
+++ Assets/VoxelEditor/GUI/EntityReferencePropertyManager.cs
@@ -4,9 +4,43 @@ using UnityEngine;
 
 public class EntityReferencePropertyManager : MonoBehaviour
 {
+    public class EntityReferenceLine : MonoBehaviour
+    {
+        public int i;
+        public Entity sourceEntity;
+        public Entity targetEntity;
+
+        private Vector3 EntityPosition(Entity entity)
+        {
+            if (entity is Substance)
+            {
+                // TODO: needs to find center of bounding box
+                foreach (Voxel voxel in ((Substance)entity).voxels)
+                    return voxel.transform.position + Vector3.one * 0.5f;
+            }
+            else if (entity is ObjectEntity)
+            {
+                return ((ObjectEntity)entity).marker.transform.position;
+            }
+            return Vector3.zero;
+        }
+
+        void Start()
+        {
+            LineRenderer line = gameObject.AddComponent<LineRenderer>();
+            line.startWidth = line.endWidth = 0.1f;
+            line.material = _lineMaterial;
+            line.startColor = line.endColor = ColorI(i);
+            line.SetPosition(0, EntityPosition(sourceEntity));
+            line.SetPosition(1, EntityPosition(targetEntity));
+        }
+    }
+
+
     private static Entity currentEntity;
     private static List<Entity> targetEntities = new List<Entity>();
 
+    private static Material _lineMaterial;
     public Material lineMaterial;
 
     public static void Reset(Entity entity)
@@ -38,49 +72,43 @@ public class EntityReferencePropertyManager : MonoBehaviour
         return entity.ToString();
     }
 
+    void Awake()
+    {
+        _lineMaterial = lineMaterial;
+    }
+
     void Update()
     {
-        // draw a line to each target entity
+        if (currentEntity == null && targetEntities.Count != 0)
+            targetEntities.Clear();
+
+        bool updateTargets = false;
+
         if (transform.childCount != targetEntities.Count)
+            updateTargets = true;
+        else
+            foreach (Transform child in transform)
+            {
+                EntityReferenceLine line = child.GetComponent<EntityReferenceLine>();
+                if (targetEntities[line.i] != line.targetEntity)
+                    updateTargets = true;
+            }
+
+        if (updateTargets)
         {
+            Debug.Log("Update targets");
             foreach (Transform child in transform)
                 Destroy(child.gameObject);
             for (int i = 0; i < targetEntities.Count; i++)
             {
                 GameObject lineObject = new GameObject();
                 lineObject.transform.parent = transform;
-                LineRenderer line = lineObject.AddComponent<LineRenderer>();
-                line.startWidth = line.endWidth = 0.1f;
-                line.material = lineMaterial;
+                EntityReferenceLine line = lineObject.AddComponent<EntityReferenceLine>();
+                line.i = i;
+                line.sourceEntity = currentEntity;
+                line.targetEntity = targetEntities[i];
             }
             return; // wait a frame to let the new/deleted objects update
         }
-        if (currentEntity == null || targetEntities.Count == 0)
-            return;
-        Vector3 sourcePosition = EntityPosition(currentEntity);
-        int j = 0;
-        foreach (Transform lineTransform in transform)
-        {
-            LineRenderer line = lineTransform.GetComponent<LineRenderer>();
-            line.startColor = line.endColor = ColorI(j);
-            line.SetPosition(0, sourcePosition);
-            line.SetPosition(1, EntityPosition(targetEntities[j]));
-            j += 1;
-        }
-    }
-
-    private Vector3 EntityPosition(Entity entity)
-    {
-        if (entity is Substance)
-        {
-            // TODO: needs to find center of bounding box
-            foreach (Voxel voxel in ((Substance)entity).voxels)
-                return voxel.transform.position + Vector3.one * 0.5f;
-        }
-        else if (entity is ObjectEntity)
-        {
-            return ((ObjectEntity)entity).marker.transform.position;
-        }
-        return Vector3.zero;
     }
 }
