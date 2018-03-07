244a85343f063e48d33aa18ec67c12fa011ffe1f
Jacob van't Hoog <vantjac@gmail.com>
2018-03-07 12:21:39 -0800

    New menu interface

---
+++ Assets/Menu/MenuGUI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGUI : GUIPanel
+{
+    public TextAsset defaultMap;
+
+    private List<string> mapFiles = new List<string>();
+
+    public override Rect GetRect(float width, float height)
+    {
+        return new Rect(width * .25f, height * .2f, width * .5f, height * .6f);
+    }
+
+    void Start()
+    {
+        UpdateMapList();
+    }
+
+    public override void WindowGUI()
+    {
+        if (GUILayout.Button("New..."))
+        {
+            NewMapGUI newMapGUI = gameObject.AddComponent<NewMapGUI>();
+            newMapGUI.handler = NewMap;
+        }
+        bool updateMapList = false;
+        foreach (string fileName in mapFiles)
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(fileName))
+                OpenMap(fileName);
+            if (GUILayout.Button("X", GUILayout.ExpandWidth(false)))
+            {
+                File.Delete(GetMapPath(fileName));
+                updateMapList = true;
+            }
+            GUILayout.EndHorizontal();
+        }
+        if (updateMapList)
+            UpdateMapList();
+    }
+
+    private void OpenMap(string name)
+    {
+        GameObject selectedMap = GameObject.Find("SelectedMap");
+        if (selectedMap == null)
+        {
+            selectedMap = new GameObject("SelectedMap");
+            selectedMap.AddComponent<SelectedMap>();
+        }
+        selectedMap.GetComponent<SelectedMap>().mapName = name;
+        SceneManager.LoadScene("editScene");
+    }
+
+    private void NewMap(string name)
+    {
+        if (name.Length == 0)
+            return;
+        string filePath = GetMapPath(name);
+        using (FileStream fileStream = File.Create(filePath))
+        {
+            using (var sw = new StreamWriter(fileStream))
+            {
+                sw.Write(defaultMap.text);
+                sw.Flush();
+            }
+        }
+        UpdateMapList();
+    }
+
+    private string GetMapPath(string name)
+    {
+        return Application.persistentDataPath + "/" + name + ".json";
+    }
+
+    private void UpdateMapList()
+    {
+        string[] files = Directory.GetFiles(Application.persistentDataPath);
+        mapFiles.Clear();
+        foreach (string name in files)
+            mapFiles.Add(Path.GetFileNameWithoutExtension(name));
+    }
+}
---
+++ Assets/Menu/NewMapGUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMapGUI : GUIPanel
+{
+    public delegate void NameHandler(string name);
+    public NameHandler handler;
+    private string mapName = "";
+
+    public override Rect GetRect(float width, float height)
+    {
+        return new Rect(width * .35f, height * .35f, width * .3f, height * .3f);
+    }
+
+    public override void WindowGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("Enter new map name...");
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        mapName = GUILayout.TextField(mapName);
+        if (GUILayout.Button("Create"))
+        {
+            handler(mapName);
+            Destroy(this);
+        }
+    }
+}
