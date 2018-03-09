bf37b77664513775a422bd29e306837109f749f0
Jacob van't Hoog <vantjac@gmail.com>
2018-03-08 17:02:17 -0800

    Add buoyancy to physics objects

--- Assets/Behaviors/Physics.cs
+++ Assets/Behaviors/Physics.cs
@@ -40,11 +40,27 @@ public class PhysicsBehavior : EntityBehavior
     }
 }
 
+// Modified from: Buoyancy.cs
+// by Alex Zhdankin
+// Version 2.1
+//
+// http://forum.unity3d.com/threads/72974-Buoyancy-script
+//
+// Terms of use: do whatever you like
+
 public class PhysicsComponent : MonoBehaviour
 {
     public float density;
     public bool gravity;
     public float volume = 1.0f;
+
+    private const float DAMPFER = 0.1f;
+    private const float VOXEL_HALF_HEIGHT = 0.5f;
+
+    private Vector3 localArchimedesForce;
+    private List<Vector3> voxels;
+    private Collider waterCollider;
+    private WaterComponent water;
 
     void Start()
     {
@@ -52,6 +68,14 @@ public class PhysicsComponent : MonoBehaviour
             OnEnable();
         else
             OnDisable();
+
+        voxels = new List<Vector3>();
+        SubstanceComponent substanceComponent = GetComponent<SubstanceComponent>();
+        if (substanceComponent != null)
+            foreach (Voxel voxel in substanceComponent.substance.voxels)
+                voxels.Add(voxel.GetBounds().center - transform.position);
+        else
+            voxels.Add(Vector3.zero);
     }
 
     void OnEnable()
@@ -75,5 +99,80 @@ public class PhysicsComponent : MonoBehaviour
         Rigidbody rigidBody = GetComponent<Rigidbody>();
         if (rigidBody != null)
             rigidBody.isKinematic = true;
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        WaterComponent cWater = c.GetComponent<WaterComponent>();
+        if (cWater == null)
+            cWater = c.transform.parent.GetComponent<WaterComponent>();
+        if (cWater != null)
+        {
+            waterCollider = c;
+            if (water != cWater)
+            {
+                water = cWater;
+                float volume = 1.0f; // in voxels
+                PhysicsComponent physics = GetComponent<PhysicsComponent>();
+                if (physics != null)
+                    volume = physics.volume;
+                volume /= 1000.0f; // because.
+                float archimedesForceMagnitude = water.density * Mathf.Abs(Physics.gravity.y) * volume;
+                localArchimedesForce = new Vector3(0, archimedesForceMagnitude, 0) / voxels.Count;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c == waterCollider)
+        {
+            water = null;
+            waterCollider = null;
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        OnTriggerEnter(c.collider);
+    }
+
+    void OnCollisionExit(Collision c)
+    {
+        OnTriggerExit(c.collider);
+    }
+
+    private float GetWaterLevel(float x, float z)
+    {
+        if (water != null)
+            return water.waterLevel;
+        return float.MinValue;
+    }
+
+    void FixedUpdate()
+    {
+        foreach (var point in voxels)
+        {
+            var wp = transform.TransformPoint(point);
+            float waterLevel = GetWaterLevel(wp.x, wp.z);
+
+            if (wp.y - VOXEL_HALF_HEIGHT < waterLevel)
+            {
+                float k = (waterLevel - wp.y) / (2 * VOXEL_HALF_HEIGHT) + 0.5f;
+                if (k > 1)
+                {
+                    k = 1f;
+                }
+                else if (k < 0)
+                {
+                    k = 0f;
+                }
+
+                var velocity = GetComponent<Rigidbody>().GetPointVelocity(wp);
+                var localDampingForce = -velocity * DAMPFER * GetComponent<Rigidbody>().mass;
+                var force = localDampingForce + Mathf.Sqrt(k) * localArchimedesForce;
+                GetComponent<Rigidbody>().AddForceAtPosition(force, wp);
+            }
+        }
     }
 }
