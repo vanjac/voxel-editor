771c18e6eb8ff148595ecceb224be9e093e02f98
Jacob van't Hoog <vantjac@gmail.com>
2018-03-30 13:34:35 -0700

    Selecting nothing clears activator target

--- Assets/VoxelEditor/GUI/PropertiesGUI.cs
+++ Assets/VoxelEditor/GUI/PropertiesGUI.cs
@@ -315,6 +315,7 @@ public class NewBehaviorGUI : GUIPanel
                     return;
                 }
                 targetEntity = null;
+                targetEntityIsActivator = false;
             };
         }
         if (typePicker != null)
