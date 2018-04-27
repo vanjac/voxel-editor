0ff3df8da812693615bf1a92def9bc7f398e8658
Jacob van't Hoog <vantjac@gmail.com>
2018-04-27 12:45:17 -0700

    Import map files! #105

---
+++ Assets/Menu/FileReceive.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FileReceive : MonoBehaviour
+{
+    public GUISkin guiSkin;
+
+    void Start()
+    {
+        ShareMap.MarkIntentUsedAndroid();
+
+        TextInputDialogGUI inputDialog = gameObject.AddComponent<TextInputDialogGUI>();
+        inputDialog.prompt = "Enter name to save imported map as...";
+        inputDialog.handler = ImportMap;
+        inputDialog.guiSkin = guiSkin;
+    }
+
+    private void Close()
+    {
+        SceneManager.LoadScene("menuScene");
+    }
+
+    private void ImportMap(string name)
+    {
+        if (name.Length == 0)
+        {
+            Close();
+            return;
+        }
+        string newPath = MenuGUI.GetMapPath(name);
+        if (File.Exists(newPath))
+        {
+            var dialog = DialogGUI.ShowMessageDialog(gameObject, "A map with that name already exists.");
+            dialog.yesButtonHandler = Close;
+            return;
+        }
+
+        try
+        {
+            using (FileStream fileStream = File.Create(newPath))
+            {
+                ShareMap.ReadSharedURLAndroid(fileStream);
+            }
+            Close();
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+            var dialog = DialogGUI.ShowMessageDialog(gameObject, "Error importing file");
+            dialog.yesButtonHandler = Close;
+        }
+    }
+}
--- Assets/Menu/Share.cs
+++ Assets/Menu/Share.cs
@@ -52,12 +52,48 @@ public class ShareMap
 
     public static string GetSharedURLAndroid()
     {
-        var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
-        var intent = activity.Call<AndroidJavaObject>("getIntent");
-        var uri = intent.Call<AndroidJavaObject>("getData");
-        if (uri == null)
-            return "";
-        return uri.Call<string>("toString");
+        using (var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var intent = activity.Call<AndroidJavaObject>("getIntent"))
+        {
+            if (intent.Call<bool>("hasExtra", "used"))
+                return "";
+            using (var uri = intent.Call<AndroidJavaObject>("getData"))
+            {
+                if (uri == null)
+                    return "";
+                return uri.Call<string>("toString");
+            }
+        }
+    }
+
+    public static void ReadSharedURLAndroid(FileStream fileStream)
+    {
+        using (var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var intent = activity.Call<AndroidJavaObject>("getIntent"))
+        using (var uri = intent.Call<AndroidJavaObject>("getData"))
+        using (var contentResolver = activity.Call<AndroidJavaObject>("getContentResolver"))
+        using (var inputStream = contentResolver.Call<AndroidJavaObject>("openInputStream", uri))
+        {
+            byte[] buffer = new byte[1024];
+            var bufferPtr = AndroidJNIHelper.ConvertToJNIArray(buffer);
+            // get the method id of InputStream.read(byte[] b, int off, int len)
+            var readMethodId = AndroidJNIHelper.GetMethodID(inputStream.GetRawClass(), "read", "([BII)I");
+            jvalue[] args = new jvalue[3];
+            // construct arguments to pass to InputStream.read
+            args[0].l = bufferPtr; // buffer
+            args[1].i = 0; // offset
+            args[2].i = buffer.Length; // length
+
+            while (true)
+            {
+                int bytesRead = AndroidJNI.CallIntMethod(inputStream.GetRawObject(), readMethodId, args);
+                if (bytesRead <= 0)
+                    break;
+                byte[] newBuffer = AndroidJNIHelper.ConvertFromJNIArray<byte[]>(bufferPtr);
+                fileStream.Write(newBuffer, 0, bytesRead);
+            }
+            fileStream.Flush();
+        }
     }
 
     public static bool CatchSharedFile()
@@ -67,5 +103,13 @@ public class ShareMap
 #else
         return false;
 #endif
+    }
+
+    public static void MarkIntentUsedAndroid()
+    {
+        using (var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var intent = activity.Call<AndroidJavaObject>("getIntent"))
+        using (intent.Call<AndroidJavaObject>("putExtra", "used", true))
+        { }
     }
 }
