70d6c0d7e25c204be67430144a3cba0c9c0c6485
Jacob van't Hoog <vantjac@gmail.com>
2018-04-26 18:31:58 -0700

    No file size limit for sharing. #105. Based on https://github.com/ChrisMaire/unity-native-sharing/issues/33#issuecomment-346729881

--- Assets/Menu/Share.cs
+++ Assets/Menu/Share.cs
@@ -5,20 +5,14 @@ using UnityEngine;
 
 // modified from https://github.com/ChrisMaire/unity-native-sharing/blob/master/Assets/Plugins/NativeShare.cs
 // and https://stackoverflow.com/a/28694269
+// and https://github.com/ChrisMaire/unity-native-sharing/issues/33#issuecomment-346729881
 
 public class ShareMap
 {
     public static void ShareAndroid(string filePath)
     {
-        string fileContent;
-        using (FileStream fileStream = File.Open(filePath, FileMode.Open))
-        {
-            using (var sr = new StreamReader(fileStream))
-            {
-                fileContent = sr.ReadToEnd();
-            }
-        }
-
+        using (AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        using (AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity"))
         using (AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent"))
         using (AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent"))
         {
@@ -26,18 +20,33 @@ public class ShareMap
             { }
             using (intentObject.Call<AndroidJavaObject>("setType", "application/json"))
             { }
+            // default file name for Google Drive
             using (intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), Path.GetFileName(filePath)))
             { }
-            // TODO: this has a length limit which is possible to exceed
-            using (intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), fileContent))
+            // default snippet name for Slack
+            using (intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), Path.GetFileName(filePath)))
             { }
 
-            using (AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-            using (AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity"))
+            using (AndroidJavaClass fileProviderClass = new AndroidJavaClass("android.support.v4.content.FileProvider"))
+            using (AndroidJavaObject unityContext = currentActivity.Call<AndroidJavaObject>("getApplicationContext"))
+            using (AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri"))
+            using (AndroidJavaObject uris = new AndroidJavaObject("java.util.ArrayList"))
             {
-                AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share map...");
-                currentActivity.Call("startActivity", jChooser);
+                string packageName = unityContext.Call<string>("getPackageName");
+                string authority = packageName + ".provider";
+
+                AndroidJavaObject fileObj = new AndroidJavaObject("java.io.File", filePath);
+                AndroidJavaObject uriObj = fileProviderClass.CallStatic<AndroidJavaObject>("getUriForFile", unityContext, authority, fileObj);
+
+                int FLAG_GRANT_READ_URI_PERMISSION = intentObject.GetStatic<int>("FLAG_GRANT_READ_URI_PERMISSION");
+                intentObject.Call<AndroidJavaObject>("addFlags", FLAG_GRANT_READ_URI_PERMISSION);
+
+                using (intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObj))
+                { }
             }
+
+            AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share map...");
+            currentActivity.Call("startActivity", jChooser);
         }
     }
 }
