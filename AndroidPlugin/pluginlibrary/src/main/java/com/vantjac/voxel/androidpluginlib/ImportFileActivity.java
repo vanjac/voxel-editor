72d71c2b728f8a43e2eac87ec9fbda0dff513a9c
Jacob van't Hoog <vantjac@gmail.com>
2018-04-28 09:57:23 -0700

    Started working on a custom android activity

---
+++ AndroidPlugin/pluginlibrary/src/main/java/com/vantjac/voxel/androidpluginlib/ImportFileActivity.java
@@ -0,0 +1,13 @@
+package com.vantjac.voxel.androidpluginlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImportFileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_file);
+    }
+}
