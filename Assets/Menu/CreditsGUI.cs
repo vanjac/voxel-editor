d1a27fd683bb1ab50dd08c395617e57869368660
Jacob van't Hoog <vantjac@gmail.com>
2018-06-21 18:50:38 -0700

    Add About button to menu with credits

---
+++ Assets/Menu/CreditsGUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsGUI : GUIPanel
+{
+    public TextAsset creditsText;
+
+    public override Rect GetRect(float width, float height)
+    {
+        return new Rect(width * .8f, 0, width * .2f, 0);
+    }
+
+    public override GUIStyle GetStyle()
+    {
+        return GUIStyle.none;
+    }
+
+    public override void OnEnable()
+    {
+        holdOpen = true;
+        stealFocus = false;
+        base.OnEnable();
+    }
+
+    public override void WindowGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if(GUILayout.Button("About", GUI.skin.GetStyle("button_large"),
+                GUILayout.ExpandWidth(false)))
+            LargeMessageGUI.ShowLargeMessageDialog(gameObject, creditsText.text);
+        GUILayout.EndHorizontal();
+    }
+}
