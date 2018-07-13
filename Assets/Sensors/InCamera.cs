2369d0d3cdd2c31f79d6bc60710ef5da36269bbb
Jacob van't Hoog <vantjac@gmail.com>
2018-07-13 11:26:03 -0700

    Add Max Distance property to In Camera sensor; #154

--- Assets/Sensors/InCamera.cs
+++ Assets/Sensors/InCamera.cs
@@ -7,36 +7,64 @@ public class InCameraSensor : Sensor
     public static new PropertiesObjectType objectType = new PropertiesObjectType(
         "In Camera", "Active when the player is looking toward the object",
         "Turns on as long as the player is looking in the direction of the object, even if it is obscured. "
-        + "Does not turn on if object doesn't have Visible behavior.",
+        + "Does not turn on if object doesn't have Visible behavior.\n\n"
+        + "Activator: player",
         "eye", typeof(InCameraSensor));
+
+    private float maxDistance = 9999;
 
     public override PropertiesObjectType ObjectType()
     {
         return objectType;
     }
 
+    public override ICollection<Property> Properties()
+    {
+        return Property.JoinProperties(new Property[]
+        {
+            new Property("Max distance",
+                () => maxDistance,
+                v => maxDistance = (float)v,
+                PropertyGUIs.Float)
+        }, base.Properties());
+    }
+
     public override SensorComponent MakeComponent(GameObject gameObject)
     {
-        return gameObject.AddComponent<InCameraComponent>();
+        var inCamera = gameObject.AddComponent<InCameraComponent>();
+        inCamera.maxDistance = maxDistance;
+        return inCamera;
     }
 }
 
 public class InCameraComponent : SensorComponent
 {
-    private bool value;
+    public float maxDistance;
+    private bool visible, inRange;
 
     public override bool IsOn()
     {
-        return value;
+        return visible && inRange;
+    }
+
+    public override EntityComponent GetActivator()
+    {
+        return PlayerComponent.instance;
+    }
+
+    void Update()
+    {
+        inRange = (PlayerComponent.instance.transform.position
+            - transform.position).magnitude <= maxDistance;
     }
 
     void OnBecameVisible()
     {
-        value = true;
+        visible = true;
     }
 
     void OnBecameInvisible()
     {
-        value = false;
+        visible = false;
     }
 }
