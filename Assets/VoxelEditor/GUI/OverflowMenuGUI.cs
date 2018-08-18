8f34167c22cecbbbc39351621bd75942f4d468a5
Jacob van't Hoog <vantjac@gmail.com>
2018-08-18 13:11:35 -0700

    Overflow menu submenus

--- Assets/VoxelEditor/GUI/OverflowMenuGUI.cs
+++ Assets/VoxelEditor/GUI/OverflowMenuGUI.cs
@@ -9,20 +9,23 @@ public class OverflowMenuGUI : GUIPanel
         public string text;
         public Texture icon;
         public System.Action action;
+        public bool stayOpen;
 
-        public MenuItem(string text, Texture icon, System.Action action)
+        public MenuItem(string text, Texture icon, System.Action action, bool stayOpen = false)
         {
             this.text = text;
             this.icon = icon;
             this.action = action;
+            this.stayOpen = stayOpen;
         }
     }
 
     public MenuItem[] items;
+    public int depth = 0;
 
     public override Rect GetRect(float width, float height)
     {
-        return new Rect(width - height * .4f, height * .13f, height * .4f, 0);
+        return new Rect(width - height * .4f * (depth + 1), height * .13f, height * .4f, 0);
     }
 
     public override GUIStyle GetStyle()
@@ -41,7 +44,15 @@ public class OverflowMenuGUI : GUIPanel
         foreach (MenuItem item in items)
         {
             if (MenuButton(item.text, item.icon))
+            {
                 item.action();
+                if (!item.stayOpen)
+                {
+                    // destroy self and all parent menus
+                    foreach(OverflowMenuGUI parentMenu in gameObject.GetComponents<OverflowMenuGUI>())
+                        Destroy(parentMenu);
+                }
+            }
         }
     }
 
@@ -51,8 +62,6 @@ public class OverflowMenuGUI : GUIPanel
         Rect iconRect = GUILayoutUtility.GetLastRect();
         iconRect.width = iconRect.height;
         GUI.Label(iconRect, icon, GUIUtils.LABEL_CENTERED.Value);
-        if (pressed)
-            Destroy(this);
         return pressed;
     }
 }
