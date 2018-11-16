dca8044a187e4e9d177e10f6f6ab153ef390a219
Jacob van't Hoog <vantjac@gmail.com>
2018-11-15 19:26:29 -0800

    Message for upgrading from an older version -- offer bevel tutorial

--- Assets/VoxelEditor/EditorFile.cs
+++ Assets/VoxelEditor/EditorFile.cs
@@ -70,7 +70,44 @@ public class EditorFile : MonoBehaviour
                 TutorialGUI.StartTutorial(Tutorials.INTRO_TUTORIAL, dialog.gameObject, voxelArray, touchListener);
             };
         }
+        else
+        {
+            string lastVersion = PlayerPrefs.GetString("last_editScene_version");
+            if (CompareVersions(lastVersion, "1.2.0") == -1)
+            {
+                var dialog = loadingGUI.gameObject.AddComponent<DialogGUI>();
+                dialog.message = "N-Space has been updated with the ability to bevel edges! Would you like a tutorial?";
+                dialog.yesButtonText = "Yes";
+                dialog.noButtonText = "No";
+                dialog.yesButtonHandler = () =>
+                {
+                    TutorialGUI.StartTutorial(Tutorials.BEVEL_TUTORIAL, dialog.gameObject, voxelArray, touchListener);
+                };
+            }
+        }
         PlayerPrefs.SetString("last_editScene_version", Application.version);
+    }
+
+    // 1: a is greater; -1: b is creater; 0: equal
+    private static int CompareVersions(string a, string b)
+    {
+        string[] aNums = a.Split('.');
+        string[] bNums = b.Split('.');
+        for (int i = 0; i < aNums.Length; i++)
+        {
+            if (i >= bNums.Length)
+                return 1;
+            int numA = System.Int32.Parse(aNums[i]);
+            int numB = System.Int32.Parse(bNums[i]);
+            if (numA > numB)
+                return 1;
+            else if (numB > numA)
+                return -1;
+        }
+        if (bNums.Length > aNums.Length)
+            return -1;
+        else
+            return 0;
     }
 
     public void Save()
