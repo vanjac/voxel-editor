4c38a2ddf5f24407d4ee8c107c552826ada3312e
Jacob van't Hoog <vantjac@gmail.com>
2019-06-06 23:59:59 -0700

    SunVoxWorldReader determines file name from file

--- Assets/Files/SunVoxWorldReader.cs
+++ Assets/Files/SunVoxWorldReader.cs
@@ -11,8 +11,19 @@ public class SunVoxWorldReader : WorldFileReader
     {
         byte[] bytes = new byte[stream.Length];
         stream.Read(bytes, 0, bytes.Length);
-        // TODO: determine file name
-        data = new EmbeddedData("imported", bytes, EmbeddedDataType.SunVox);
+
+        string name = null;
+        int slot = SunVoxUtils.OpenUnusedSlot();
+        int result = SunVox.sv_load_from_memory(slot, bytes, bytes.Length);
+        if (result == 0)
+            name = System.Runtime.InteropServices.Marshal.PtrToStringAuto(SunVox.sv_get_song_name(slot));
+        SunVoxUtils.CloseSlot(slot);
+        if (name != null)
+            name = name.Trim();
+        if (name == null || name == "")
+            name = "imported";
+
+        data = new EmbeddedData(name, bytes, EmbeddedDataType.SunVox);
     }
 
     public List<string> BuildWorld(Transform cameraPivot, VoxelArray voxelArray, bool editor)
