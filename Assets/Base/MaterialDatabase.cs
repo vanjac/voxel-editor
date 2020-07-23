ea9d3a8a738436abcf2fd8d00ccef2ae50483c0e
Jacob van't Hoog <vantjac@gmail.com>
2020-07-22 21:38:03 -0700

    Assign sounds to materials

--- Assets/Base/MaterialDatabase.cs
+++ Assets/Base/MaterialDatabase.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public enum MaterialSound
 {
     GENERIC, CONCRETE, ROCK, PLASTER, FABRIC, DIRT, GRASS, GRAVEL, SAND, METAL,
-    TILE, SNOW, ICE, WOOD, METAL_GRATE, GLASS, WATER
+    TILE, SNOW, ICE, WOOD, METAL_GRATE, GLASS, WATER, CHAIN_LINK
 }
 
 // could represent a material or a directory!
---
+++ Assets/Game/FootstepSounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSounds : MonoBehaviour
+{
+    [System.Serializable]
+    public struct FootstepSoundEntry
+    {
+        public MaterialSound sound; // unused!
+        public float volume;
+        public AudioClip[] left, right;
+    }
+    public List<FootstepSoundEntry> sounds = new List<FootstepSoundEntry>();
+}
