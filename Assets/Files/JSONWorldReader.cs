d33e8ae71b02300eaee0b47fa39a61ac21b0e894
j v <vantjac@gmail.com>
2022-02-15 16:15:45 -0800

    Update SimpleJSON

--- Assets/Files/JSONWorldReader.cs
+++ Assets/Files/JSONWorldReader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using SimpleJSON;
-using System.Xml;
 using System.Xml.Serialization;
 
 public class JSONWorldReader : WorldFileReader
