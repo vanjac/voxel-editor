abe712ada98318830ec99b55e503e040c88be023
j v <vantjac@gmail.com>
2023-07-06 00:41:07 -0700

    Scale behavior

---
+++ Assets/Behaviors/Scale.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBehavior : EntityBehavior
+{
+    public static new BehaviorType objectType = new BehaviorType(
+        "Scale", "Change size",
+        "resize", typeof(ScaleBehavior),
+        BehaviorType.AndRule(
+            BehaviorType.BaseTypeRule(typeof(DynamicEntity)),
+            BehaviorType.NotBaseTypeRule(typeof(PlayerObject))));
+
+    private Vector3 scale = Vector3.one;
+
+    public override BehaviorType BehaviorObjectType()
+    {
+        return objectType;
+    }
+
+    public override ICollection<Property> Properties()
+    {
+        return Property.JoinProperties(base.Properties(), new Property[]
+        {
+            new Property("sca", "Factor",
+                () => scale,
+                v => scale = (Vector3)v,
+                PropertyGUIs.Vector3),
+        });
+    }
+
+    public override Behaviour MakeComponent(GameObject gameObject)
+    {
+        var component = gameObject.AddComponent<ScaleComponent>();
+        component.scale = scale;
+        return component;
+    }
+}
+
+public class ScaleComponent : BehaviorComponent
+{
+    public Vector3 scale;
+    private Vector3 storedScale;
+
+    public override void BehaviorEnabled()
+    {
+        storedScale = transform.localScale;
+        transform.localScale = scale;
+    }
+
+    public override void BehaviorDisabled()
+    {
+        transform.localScale = storedScale;
+    }
+}
